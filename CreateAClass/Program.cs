00d5be0f2a41b45b994e055f6da928f3698532b7
Angelo Bovino <angelo.bovino@outlook.com>
2019-09-01 14:19:24 -0400

    Basic class example

---
+++ CreateAClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Angelo", "Bovino", 29, 99.99);
+            s1.SayHello();
+
+            Console.WriteLine(s1.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CreateAClass/Student.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAClass
+{
+    class Student
+    {
+        private string name;
+        private int age;
+        private double finalGrade;
+        // Auto-Implemented Property
+        public string LastName { get; private set; }
+
+        public Student(string name, string lastName, int age, double finalGrade)
+        {
+            this.name = name;
+            this.LastName = lastName;
+            this.age = age;
+            this.finalGrade = finalGrade;
+        }
+
+        public Student(string name, string lastName, int age) 
+            : this(name, lastName, age, 0)
+        {
+            
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hi, my name is {this.name}");
+        }
+
+        // Property
+        public string Name
+        {
+            get
+            {
+                if (this.age >= 18)
+                {
+                    return this.name;
+                }
+                else
+                {
+                    return "This student is too young to display info";
+                }
+            }
+
+            set
+            {
+                if (value != "")
+                {
+                    this.name = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                if (this.age >= 18)
+                {
+                    return this.age;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            set
+            {
+                if (value != 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public double FinalGrade
+        {
+            get
+            {
+                return this.finalGrade;
+            }
+
+            set
+            {
+                if (value < 65)
+                {
+                    this.finalGrade= 65;
+                }
+                else if (value > 100)
+                {
+                    this.finalGrade = 100;
+                }
+                else
+                {
+                    this.finalGrade = value;
+                }
+            }
+        }
+
+    }
+}
